6adddf94b6dd3676408c54888680918c2192fb85
Raphael Feijó <824135407@ulife.com.br>
2025-08-22 11:21:09 -0300

    conhecendo variaveis em java script

---
+++ variaveis.js
@@ -0,0 +1,43 @@
+var nome = "Raphael";
+const altura = 1.82
+let idade = 19
+var lana = "Lana Del Rey"
+console.log("Meu nome é: " + nome + " Tenho: " + idade + " anos " + " E: " + altura + " Escuto: " + lana);
+
+// String
+
+let texto = "String"
+
+// Number
+let num = 10
+
+//boolean
+let boo = true
+let boo2 = false
+
+//null 
+let nulo = null
+
+//undefined
+let indef = undefined
+
+// object
+let objeto = {nome: "Raphael", idade: 19, altura: 1.82}
+
+//array
+let lista = ["uva", "maça", "banana", "lapis "]
+
+//Conversão
+
+// String to number
+let stringToNumber = number("10")
+let stringToNumber2 = parseInt("10.5") //converte para inteiro
+let stringToNumber3 = parseFloat("10.5") //converte para float
+
+// Number To String
+let str = String(100);
+let str2 = (100).toString();
+
+//boolean to number
+let booleanoToNumber = Number(true)//1
+let booleanoToNumber2 = Number(false)//0
