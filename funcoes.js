9abe0af3140f41b7900a21fbd77005db8c527977
Raphael Feijó <824135407@ulife.com.br>
2025-08-22 11:48:27 -0300

    conhecendo funções em java script

---
+++ funcoes.js
@@ -0,0 +1,27 @@
+// Função declarativa
+function saudacao(nome) {
+    return `olá, ${nome}!`
+}
+console.log(saudacao("raphael"))
+
+//funçõa anonima = sem nome
+const soma = function(a, b) {
+    return a + b;
+}
+//Merda
+function simples(){
+    let a = 10;
+    let b = 10;
+    let c = a + b
+    console.log(("O valor de C é: " + c))
+}
+console.log(("O valor de C é: " + c))
+simples()
+
+function somar(a, b){
+    return a + b;
+} 
+
+console.log(somar(5, 10))
+
+//função 
